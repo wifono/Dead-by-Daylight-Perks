fcca67ac4f4d9fac0f9867bf827dc690f947dba9
wifono <daniel.trstansky@gmail.com>
2021-10-26 17:19:46 +0200

    ADD Bug report

---
+++ src/Home.svelte
@@ -0,0 +1,111 @@
+
+<script>
+    
+import {onMount}  from 'svelte';
+
+import {bookData} from "./bookData.js";
+import Book from "./components/Book.svelte";
+import Menu from "./components/Menu.svelte";
+import Search from "./components/Search.svelte";
+import Logo from "./components/Logo.svelte";
+import Or from "./components/Or.svelte";
+import Info from "./components/Info.svelte";
+import Error from "./components/Error.svelte";
+import { flip } from 'svelte/animate'
+
+let names =  [];
+let selectedName = "";
+const getNames = () => {
+	for (let bookObj of bookData) {
+		if (!names.includes(bookObj.name)) {
+			names = [...names, bookObj.name]		
+		}
+	}
+	names = names.sort()
+}
+onMount(() => getNames());
+
+let filteredBooks = [];
+$: if (selectedName) getPerkByName();
+
+// For select menu
+const getPerkByName = () => {
+
+	searchTerm = "";
+
+	if (selectedName === 'All') {
+		return filteredBooks = bookData;
+	
+	}
+	return filteredBooks = bookData.filter(bookObj => {
+		return bookObj.name === selectedName;
+	})
+}
+// Search input
+let searchTerm = "";
+$: if (searchTerm) selectedName = "";
+
+const searchPerks = () => {
+	
+		return filteredBooks = bookData.filter(bookObj => {
+		let bookTitleLower = bookObj.perk_name.toLowerCase();
+		return bookTitleLower.includes(searchTerm.toLowerCase());
+	})
+}
+
+
+
+$:console.log(selectedName, filteredBooks);
+$:console.log(searchTerm)
+</script>
+
+<header>
+	<Logo />
+</header>
+
+<div class="navigation">
+	<Menu {names} bind:selectedName />
+	<Or />
+	<Search bind:searchTerm on:input={searchPerks}/>
+</div>
+
+<main id="bookshelf">
+
+	{#if  searchTerm.length === 0 && selectedName.length === 0}
+	
+		<Info />
+	
+	{:else if filteredBooks.length > 0}
+		{#each filteredBooks as {name, perk_name, description, icon_url}}
+		<Book 
+			{name}
+			{perk_name}
+			{description}
+			{icon_url} />
+		{/each}
+	{:else if searchTerm && filteredBooks.length === 0}
+	<Error />
+
+	{/if}
+</main>
+
+
+<style>
+	.navigation {
+		width: 50vw;
+		display: flex;
+		justify-content:center;
+		align-items:baseline;
+		margin: 2em 0 .5em 0;
+		;
+	}
+
+	
+    @media screen and (max-width:324px) {
+        .navigation {
+			margin: 0 0 1em 0;
+		}
+    }
+            
+
+</style>
---
+++ src/components/Navig.svelte
@@ -0,0 +1,45 @@
+<script>
+	
+</script>
+
+<div class="nav-container">
+	<nav>
+		<a href="/#/">Home</a>
+		<a href="#/reports">Bug Reports</a>
+	</nav>
+</div>
+
+<style>
+
+	.nav-container {
+		position: fixed;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		z-index: 50;
+		top: 0;
+		width: 100%;
+
+		padding: 1em 0;
+		
+		background: rgba(22, 22, 22, 0.644);
+	}
+
+	nav {
+		font-size: 2em;
+		font-family: "Amatic SC", cursive;	
+	}
+
+	a {
+		text-decoration: none;
+		color: aliceblue;
+		padding: 0.5em; 
+	}
+	
+	a:hover {
+		color: rgb(255, 0, 0);
+		transition: all .2s ease-in;
+	}
+    
+</style>
